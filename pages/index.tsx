43e8d4112a019707a046a3255d400b76bc50946e
Paul Maier <pauljustus279@gmail.com>
2022-06-22 20:52:36 +0200

    Removed debug button and made side label more responsive

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,19 +2,11 @@ import Layout from "../components/layout"
 import styles from "./styles/index.module.css"
 import schulmaterial from "../assets/schulmaterial.png"
 import Image from "next/image"
-import { useToasts } from 'react-toast-notifications'
 
 export default function IndexPage() {
-  const { addToast } = useToasts()
 
   return (
     <Layout>
-      <button onClick={() => addToast('Success', {
-        appearance: 'success',
-        autoDismiss: true,
-      })}>
-        Add Toast
-      </button>
       <h1 className={styles.h1}>Willkommen in der Materialausgabe des Birklehof</h1>
       <div className={styles.image}>
         <Image layout="intrinsic" src={schulmaterial} alt="Schulmaterial" />
